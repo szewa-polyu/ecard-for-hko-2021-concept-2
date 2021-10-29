e0619d8945fe9bdb915222d356b453cee59b2d34
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2021-10-29 13:27:04 +0800

    added audio and button

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,13 +1,13 @@
-import { useRef, useEffect, useCallback, useState } from 'preact/hooks';
+import { useRef, useEffect, useState, useCallback } from 'preact/hooks';
 import Paper from 'paper';
 import style from './style.css';
 
 // https://stackoverflow.com/questions/56197908/how-to-use-paperjs-with-react
+// http://paperjs.org/tutorials/images/using-pixel-colors/
 
-const { Size, Point, Raster, Path, Color, Tween } = Paper;
+const { Size, Point, Raster, Path, Color } = Paper;
 
-const canvasWidth = 550;
-const canvasHeight = 320;
+/* utils */
 
 const loadRasterAsync = (src, onBeforeLoad = null) =>
   new Promise((resolve, reject) => {
@@ -25,63 +25,98 @@ const loadRasterAsync = (src, onBeforeLoad = null) =>
     raster.on('load', handleLoad);
   });
 
-const Canvas = _ => {
-  const [isPlay, setIsPlay] = useState(false);
-  const [isShowResetButton, setIsShowResetButton] = useState(false);
+/* end of utils */
 
+/* constants */
+
+// canvas size
+const canvasWidth = 550;
+const canvasHeight = 320;
+
+// The size of our grid cells:
+const gridSize = 10;
+
+// Space the cells by 120%:
+const spacing = 1.2;
+
+const numOfGridX = canvasWidth / gridSize;
+const numOfGridY = canvasHeight / gridSize;
+const totalNumOfGrids = numOfGridX * numOfGridY;
+const layer1FadeInCounterThreshold = 0.05 * totalNumOfGrids;
+
+const layer1FadeInTween1Duration = 2500;
+const layer1FadeInTween2Duration = 2500;
+
+const rasterSrc = process.env.PUBLIC_PATH + 'assets/images/background.jpg';
+
+// button text
+let buttonText = 'Turn volume on & play on the canvas!';
+const pathname = window.location.pathname;
+if (pathname.includes('index_tc.html')) {
+  buttonText = '打開音量並在畫布上遊玩！';
+}
+if (pathname.includes('index_sc.html')) {
+  buttonText = '打开音量并在画布上游玩！';
+}
+
+// styles
+const hideStyle = {
+  opacity: 0,
+  visibility: 'hidden'
+};
+
+const showStyle = {
+  opacity: 1,
+  visibility: 'visible'
+};
+
+/* end of constants */
+
+const Canvas = _ => {
   const canvasRef = useRef(null);
   const audioRef = useRef(null);
 
+  const layer1Raster = useRef(null);
+  const layer2And3Raster = useRef(null);
+  const layer2And3Paths = useRef([]);
+  const layer2And3FlippedCounter = useRef(0);
+  const isLayer1FadedIn = useRef(false);
+
+  const [isStarted, setIsStarted] = useState(false);
+
   useEffect(async _ => {
     Paper.setup(canvasRef.current);
-
     const { view, project } = Paper;
 
-    // http://paperjs.org/tutorials/images/using-pixel-colors/
-
-    // The size of our grid cells:
-    const gridSize = 10;
-
-    // Space the cells by 120%:
-    const spacing = 1.2;
-
-    const numOfGridX = canvasWidth / gridSize;
-    const numOfGridY = canvasHeight / gridSize;
-    const totalNumOfGrids = numOfGridX * numOfGridY;
-    const layer1FadeInCounterThreshold = 0.05 * totalNumOfGrids;
-
-    const layer1FadeInTween1Duration = 2500;
-    const layer1FadeInTween2Duration = 2500;
-
-    const rasterSrc = process.env.PUBLIC_PATH + 'assets/images/background.jpg';
+    /* setting up layers */
 
     // layer 1
-    const layer1Raster = await loadRasterAsync(rasterSrc, raster => {
+    layer1Raster.current = await loadRasterAsync(rasterSrc, raster => {
       raster.position = view.center;
     });
 
     // set up layer 1
-    layer1Raster.size = new Size(canvasWidth, canvasHeight);
+    layer1Raster.current.size = new Size(canvasWidth, canvasHeight);
 
-    layer1Raster.opacity = 0;
+    layer1Raster.current.opacity = 0;
 
     // layers 2 and 3
-    const layer2And3Raster = await loadRasterAsync(rasterSrc, raster => {
+    layer2And3Raster.current = await loadRasterAsync(rasterSrc, raster => {
       raster.visible = false;
     });
 
-    const layer2And3Paths = [];
+    layer2And3Paths.current = [];
 
     // set up layers 2 & 3
 
     // Since the example image we're using is much too large,
     // and therefore has way too many pixels, lets downsize it to
-    layer2And3Raster.size = new Size(numOfGridX, numOfGridY);
+    layer2And3Raster.current.size = new Size(numOfGridX, numOfGridY);
 
-    for (let y = 0; y < layer2And3Raster.height; y++) {
-      for (let x = 0; x < layer2And3Raster.width; x++) {
+    for (let y = 0; y < numOfGridY; y++) {
+      for (let x = 0; x < numOfGridX; x++) {
         // Get the color of the pixel:
-        const color = layer2And3Raster.getPixel(x, y);
+        const color = layer2And3Raster.current.getPixel(x, y);
 
         // set up layer 2
 
@@ -109,7 +144,7 @@ const Canvas = _ => {
         // Scale the path by the amount of gray in the pixel color:
         layer3Path.scale(1 - color.gray);
 
-        layer2And3Paths.push({
+        layer2And3Paths.current.push({
           layer2: layer2Path,
           layer3: layer3Path,
           isFlipped: false
@@ -121,122 +156,154 @@ const Canvas = _ => {
     // the created paths in it appear centered.
     project.activeLayer.position = view.center;
 
-    let isLayer1FadedIn = false;
+    /* end of setting up layers */
 
-    const fadeInLayer1 = _ => {
-      isLayer1FadedIn = true;
-      console.log('fade in!');
+    /* initiating refs */
 
+    setIsStarted(false);
+    layer2And3FlippedCounter.current = 0;
+    isLayer1FadedIn.current = false;
+
+    /* end of initiating refs */
+
+    view.draw();
+  }, []);
+
+  /* functions and event handlers */
+
+  const fadeInLayer1 = useCallback(_ => {
+    isLayer1FadedIn.current = true;
+    console.log('fade in!');
+    audioRef.current.volume = 0;
+    audioRef.current.play();
+
+    // dummy layer2And3Raster
+    const tween1 = layer2And3Raster.current.tween(
+      { myValue: 0 },
+      { myValue: 1 },
+      {
+        duration: layer1FadeInTween1Duration,
+        easing: 'easeOutCubic'
+      }
+    );
+
+    tween1.on('update', ({ factor }) => {
+      const layer2And3PathsCurrent = layer2And3Paths.current;
+      for (let layer2And3Path of layer2And3PathsCurrent) {
+        layer2And3Path.layer3.fillColor.alpha = 1 - factor;
+        layer2And3Path.layer2.fillColor.alpha = 0.5 + 0.5 * factor;
+      }
+      audioRef.current.volume = 0.5 * factor;
+    });
+
+    tween1.then(_ => {
       // dummy layer2And3Raster
-      const tween1 = layer2And3Raster.tween(
+      const tween2 = layer2And3Raster.current.tween(
         { myValue: 0 },
         { myValue: 1 },
         {
-          duration: layer1FadeInTween1Duration,
+          duration: layer1FadeInTween2Duration,
           easing: 'easeOutCubic'
         }
       );
 
-      tween1.on('update', ({ factor }) => {
-        for (let layer2And3Path of layer2And3Paths) {
-          layer2And3Path.layer3.fillColor.alpha = 1 - factor;
-          layer2And3Path.layer2.fillColor.alpha = 0.5 + 0.5 * factor;
+      tween2.on('update', ({ factor }) => {
+        const layer2And3PathsCurrent = layer2And3Paths.current;
+        for (let layer2And3Path of layer2And3PathsCurrent) {
+          layer2And3Path.layer2.fillColor.alpha = 1 - factor;
         }
+        layer1Raster.current.opacity = factor;
+        audioRef.current.volume = 0.5 + 0.5 * factor;
       });
+    });
+  }, []);
 
-      tween1.then(_ => {
-        // dummy layer2And3Raster
-        const tween2 = layer2And3Raster.tween(
-          { myValue: 0 },
-          { myValue: 1 },
-          {
-            duration: layer1FadeInTween2Duration,
-            easing: 'easeOutCubic'
-          }
+  const handleMouseMove = useCallback(event => {
+    const { x, y } = event.point;
+    let gridX = Math.round(x / gridSize);
+    let gridY = Math.round(y / gridSize);
+
+    if (gridX >= numOfGridX) {
+      gridX = numOfGridX - 1;
+    }
+
+    if (gridY >= numOfGridY) {
+      gridY = numOfGridY - 1;
+    }
+
+    const pixelIdx = gridY * numOfGridX + gridX;
+
+    const layer2And3Path = layer2And3Paths.current[pixelIdx];
+
+    if (layer2And3Path === undefined) {
+      console.log('Exception: ', gridX, gridY);
+    } else {
+      if (!layer2And3Path.isFlipped) {
+        layer2And3Path.layer3.fillColor.alpha = 0;
+        layer2And3Path.layer2.fillColor.alpha = 1;
+        layer2And3Path.isFlipped = true;
+
+        const layer2And3FlippedCounterTemp =
+          layer2And3FlippedCounter.current + 1;
+        layer2And3FlippedCounter.current = layer2And3FlippedCounterTemp;
+
+        console.log(
+          'fill progress: ',
+          Math.ceil(
+            (layer2And3FlippedCounterTemp / layer1FadeInCounterThreshold) * 100
+          ),
+          '%'
         );
 
-        tween2.on('update', ({ factor }) => {
-          for (let layer2And3Path of layer2And3Paths) {
-            layer2And3Path.layer2.fillColor.alpha = 1 - factor;
-          }
-          layer1Raster.opacity = factor;
-        });
-      });
-    };
-
-    let layer2And3FlippedCounter = 0;
-
-    const handleMouseMove = event => {
-      const { x, y } = event.point;
-      let gridX = Math.round(x / gridSize);
-      let gridY = Math.round(y / gridSize);
-
-      if (gridX >= numOfGridX) {
-        gridX = numOfGridX - 1;
-      }
-
-      if (gridY >= numOfGridY) {
-        gridY = numOfGridY - 1;
-      }
-
-      const pixelIdx = gridY * layer2And3Raster.width + gridX;
-
-      const layer2And3Path = layer2And3Paths[pixelIdx];
-
-      if (layer2And3Path === undefined) {
-        console.log('Exception: ', gridX, gridY);
-      } else {
-        if (!layer2And3Path.isFlipped) {
-          layer2And3Path.layer3.fillColor.alpha = 0;
-          layer2And3Path.layer2.fillColor.alpha = 1;
-          layer2And3Path.isFlipped = true;
-
-          layer2And3FlippedCounter++;
-
-          console.log(
-            'fill progress: ',
-            Math.ceil(
-              (layer2And3FlippedCounter / layer1FadeInCounterThreshold) * 100
-            ),
-            '%'
-          );
-
-          if (
-            !isLayer1FadedIn &&
-            layer2And3FlippedCounter > layer1FadeInCounterThreshold
-          ) {
-            view.off('mousemove', handleMouseMove);
-            fadeInLayer1();
-          }
+        if (
+          !isLayer1FadedIn.current &&
+          layer2And3FlippedCounterTemp > layer1FadeInCounterThreshold
+        ) {
+          deregisterMouseMove();
+          fadeInLayer1();
         }
       }
-    };
-
-    view.on('mousemove', handleMouseMove);
-
-    view.onMouseMove = view.draw();
+    }
   }, []);
 
-  useEffect(
+  const registerMouseMove = useCallback(
     _ => {
-      if (isPlay) {
-        audioRef.current.play();
-      }
+      const { view } = Paper;
+      view.on('mousemove', handleMouseMove);
     },
-    [isPlay]
+    [handleMouseMove]
   );
 
-  const handleResetButtonClick = useCallback(_ => {
-    setIsPlay(false);
-    setIsShowResetButton(false);
-    audioRef.current.pause();
-    audioRef.current.currentTime = 0;
+  const deregisterMouseMove = useCallback(
+    _ => {
+      const { view } = Paper;
+      view.off('mousemove', handleMouseMove);
+    },
+    [handleMouseMove]
+  );
+
+  const handleStartButtonClick = useCallback(_ => {
+    registerMouseMove();
+    setIsStarted(true);
   }, []);
+
+  /* end of functions and event handlers */
 
   return (
     <div className={style.canvasContainer}>
       <canvas ref={canvasRef} className={style.ecardCanvas} />
-      <audio ref={audioRef} src={''} loop={true} />
+      <button
+        className={style.startButton}
+        onClick={handleStartButtonClick}
+        style={isStarted ? hideStyle : showStyle}
+      >
+        {buttonText}
+      </button>
+      <audio
+        ref={audioRef}
+        src={process.env.PUBLIC_PATH + 'assets/audio/bgm.mp3'}
+        loop={true}
+      />
     </div>
   );
 };
